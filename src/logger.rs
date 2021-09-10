e4c30eb25803aa68bfe762619a50165cafe3c299
James Teow <james.teow83@gmail.com>
2021-09-10 12:52:32 -0400

    Refactor code

---
+++ src/logger.rs
@@ -0,0 +1,35 @@
+use std::{fs::File, io::Write, fs::OpenOptions};
+use std::io;
+
+extern crate chrono;
+use chrono::*;
+
+fn create_file(filename: &'static str) -> Result<File, io::Error> {
+  let f =  OpenOptions::new().append(true).open(filename)?;
+  Ok(f)
+}
+
+fn log_string(filename: &'static str, string: &[u8]) -> std::io::Result<()> {
+  let mut buffer = create_file(filename).unwrap();
+  let mut pos = 0;
+
+  while pos < string.len() {
+      let bytes_written = buffer.write(&string[pos..])?;
+      pos += bytes_written;
+  }
+
+  Ok(())
+  
+}
+
+fn create_time_entry() -> String {
+  let local: DateTime<Local> = Local::now();
+  local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string()
+}
+
+pub fn log_time(str: &'static str) -> io::Result<()> {
+  let entry = create_time_entry();
+  let bytes = entry.as_bytes();
+  log_string(str, bytes)?;
+  Ok(())
+}
