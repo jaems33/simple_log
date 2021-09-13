e9469e97907b8aabb1fa31ab47ff50cc0dcd341a
James Teow <james.teow83@gmail.com>
2021-09-13 00:48:21 -0400

    Added CLI capability

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,15 @@
 use std::{fs::File, io::Write, fs::OpenOptions};
-use std::io;
+use std::{io, string};
 
 extern crate chrono;
 use chrono::*;
 
-fn create_file(filename: &'static str) -> Result<File, io::Error> {
-  let f =  OpenOptions::new().append(true).open(filename)?;
+fn create_file(filename: String) -> Result<File, io::Error> {
+  let f =  OpenOptions::new().create(true).append(true).open(filename)?;
   Ok(f)
 }
 
-fn log_string(filename: &'static str, string: &[u8]) -> std::io::Result<()> {
+fn log_string(filename: String, string: &[u8]) -> std::io::Result<()> {
   let mut buffer = create_file(filename).unwrap();
   let mut pos = 0;
 
@@ -27,7 +27,7 @@ fn create_time_entry() -> String {
   local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string()
 }
 
-pub fn log_time(str: &'static str) -> io::Result<String> {
+pub fn log_time(str: String) -> io::Result<String> {
   let entry = create_time_entry();
   let bytes = entry.as_bytes();
   log_string(str, bytes)?;
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
-use nickel::{MiddlewareResult, Nickel, Request, Response};
+#[macro_use]
+extern crate nickel;
+
+use nickel::{Middleware, MiddlewareResult, Nickel, Request, Response};
 use tokio;
+use clap::{Arg, App};
+
 mod logger;
 
-fn log(_req: &mut Request, res: Response) -> MiddlewareResult {
-    match logger::log_time("log.txt") {
-        Ok(s) => {
-            let str = format!("Logged the time: {}", s);
-            res.send(str)
-        },
-        Err(e) => {
-            let str = format!("Error creating file{}", e);
-            res.send(str)
-        }
-    }
+fn logger_fn(req: &mut Request<String>, res: Response<String>) -> MiddlewareResult<String> {
+    let str = req.server_data().to_string();
+    let output = match logger::log_time(str) {
+            Ok(s) => {
+                format!("Logged the time: {}", s)
+            },
+            Err(e) => {
+                format!("Error creating file{}", e)
+            }
+        };
+    res.send(output.clone())
 }
 
-#[tokio::main] 
-async fn main() {
-    let mut server = Nickel::new();
-    server.utilize(log);
-    // server.get("**", log);
-    server.listen("127.0.0.1:6767").await.unwrap();
 
+#[tokio::main] 
+async fn main(){
+
+    let matches = App::new("Simple Log")
+        .version("0.1")
+        .author("James T. <james.teow33@gmail.com>")
+        .about("A simple app to log running the app.")
+        .arg(Arg::new("log_file")
+            .about("Sets the filename to save logs to.")
+            .long("logfile")
+            .short('l')
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::new("auth_token")
+            .about("Optional set token")
+            .long("token")
+            .short('t')
+            .takes_value(true)
+            .required(false))
+        .get_matches();
+    
+    let log_file = matches.value_of("log_file").unwrap().to_string();
+    let mut server = Nickel::with_data(log_file);
+    server.utilize(logger_fn);
+    server.listen("127.0.0.1:6767").await.unwrap();
+    
 }
